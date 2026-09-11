53e558cec616bcb1fbf5c9ddc9bec3170836a16a
AmbikaPattem <ambikapattem5@gmail.com>
2026-09-11 21:12:49 +0530

    done with few features in restaurant card and home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
+import type { Restaurant } from "../../types/Restaurant";
+import { Restaurants } from "../../services/restaurantData";
+import RestaurantCard from "../../Components/RestaurantCard";
+import { useNavigate } from "react-router-dom";
+import './Home.css'
 function Home(){
+    const [searchInput,setSearchInput]=useState<string>("");
+   // console.log(Restaurants)
+   const navigate=useNavigate();
+    function handleSearch(e){
+         return setSearchInput(e.target.value);
+    }
+    const filteredResult=Restaurants.filter((restaurant)=>{
+        return (restaurant.restaurantName).toLowerCase().includes(searchInput.toLowerCase())
+        
+})
+const cuisinList=Restaurants.map((restarant)=>
+{
+    return restarant.cuisine;
+})
+let uniqueList=[];
+for(let i=0;i<cuisinList.length;i++){
+    let subList=cuisinList[i];
+    for(let j=0;j<subList.length;j++){
+        let cuisinItem=subList[j];
+        if(!uniqueList.includes(cuisinItem)){
+            uniqueList.push(cuisinItem)
+        }
+    }
+}
+function handleCard({restaurant}){
+    navigate(`/restaurantCard/${restaurant.id}`,{state:restaurant})
+
+}
+console.log(uniqueList)
+console.log(cuisinList);
     return(
         <div>
-            Home
+            <div>
+                <section>
+                    <div>
+                        <h4>Delicious food, delivered to your door 🍕</h4>
+                        <p>Discover restaurants and delicious meals near you</p>
+                    </div>
+                 <div>
+                    <input type="text" value={searchInput} onChange={handleSearch} placeholder="Search restaurants or cuisines..."/>
+                    {searchInput.trim()!=""&&(
+                    filteredResult.length>0?filteredResult.map((restaurant)=>
+                    <p>{restaurant.restaurantName}</p>
+                    ):<p>"NO Items Found"</p>
+                    )
+            }
+                </div>                                        
+                </section>
+            </div>
+            <div>
+                <h4>Popular Cuisines</h4>
+             {uniqueList.map((cuisin)=>(
+                <button>{cuisin}</button>
+             ))}
+            </div>
+            <div>
+                <h4>Restaurants near you</h4>
+                <div className="restaurentList">
+                {Restaurants.map((restaurant)=>( 
+                    <div key={restaurant.id} onClick={()=>handleCard({restaurant})} className="card">
+                       <img src={restaurant.restaurantImage} alt="image" height={50} width={100}/>
+                       <h6>{restaurant.restaurantName}</h6>
+                       <p><small>⭐ {restaurant.rating}</small></p>
+                       <p><small>{restaurant.deliveryTime} minutes</small></p>
+                    </div>
+                ))}
+                </div>
+            </div>
         </div>
     )
 }
--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -8,6 +8,7 @@ import Login from "../Pages/Login/Login";
 import Restaurants from "../Pages/Restaurants/Restaurants";
 import Cart from "../Components/Cart/Cart";
 import ForgotPassword from "../Pages/ForgotPassword/ForgotPassword";
+import RestaurantCard from "../Components/RestaurantCard";
 function AppRoutes(){
     return(
         <Routes>
@@ -22,6 +23,7 @@ function AppRoutes(){
             <Route path='login'element={<Login/>}/>
             <Route path='forgotPassword' element={<ForgotPassword/>}/>
             </Route>
+            <Route path='/restaurantCard/:id' element={<RestaurantCard/>}/>
         </Routes>
     )
 }
