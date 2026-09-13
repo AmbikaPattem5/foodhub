1d1aa74311e2262f4cf54dd63f1baf40fb312ddb
AmbikaPattem <ambikapattem5@gmail.com>
2026-09-13 17:41:38 +0530

    final changes done in cart

--- src/Context/Cart/CartProvider.tsx
+++ src/Context/Cart/CartProvider.tsx
@@ -3,54 +3,66 @@ import { useState } from "react";
 import type { CartItemType, ChildrenProp } from "../../types/Types";
 function CartProvider({children} : ChildrenProp){
     const [cartItem,setCartItem]=useState<CartItemType[]>([]);
-    const updatedCartArray=[...cartItem]
-    function handleAddItem(item:CartItemType){
-        const itemExists : boolean = cartItem.some((cart)=>(cart.id === item.id));
-        if(itemExists){
-             updatedCartArray.forEach((cart) => {
-                if(cart.id === item.id){
-                     cart.quantity =Number(cart.quantity) + 1;
-                    }
+    function handleAddItem(item: RestaurantMenu) {
+  const itemExists = cartItem.some((cart) => cart.id === item.id);
 
-                })
-                setCartItem([...updatedCartArray]);
-    
-        }
-        else{
-            
-            const newCart = {...item}
-            newCart.quantity = 1 ;
-            setCartItem([...updatedCartArray,newCart]);
-        }
-        console.log(cartItem);
-    }
+  if (itemExists) {
+    const updatedCart = cartItem.map((cart) => {
+      if (cart.id === item.id) {
+        return {
+          ...cart,
+          quantity: cart.quantity + 1,
+        };
+      }
+
+      return cart;
+    });
+
+    setCartItem(updatedCart);
+  } else {
+    const newCartItem: CartItemType = {
+      id: item.id,
+      restaurantId: item.restaurantId,
+      name: item.name,
+      price: item.price,
+      quantity: 1,
+    };
+
+    setCartItem([...cartItem, newCartItem]);
+  }
+}
     function removeItem(itemId : number){
-            const result : CartItemType[] = cartItem.filter((cart)=>(cart.id !== item.id))
+            const result : CartItemType[] = cartItem.filter((cart)=>(cart.id !== itemId))
             setCartItem(result);
 
     }
     function incrementCartItem(itemId : number){
-        updatedCartArray.forEach((item) => { 
+            const updatedCartArray = cartItem.map((item)=>{
             if(item.id === itemId)
             {
-                item.quantity = item.quantity + 1;
+                return {...item, quantity : item.quantity + 1}
             } 
+             return item
      } )
      setCartItem([...updatedCartArray])
 
     }
-    function decrementCartItem(itemId : number){
-        updatedCartArray.forEach((item) => {
-            
-            if(item.id === itemId){
-                item.quantity = item.quantity - 1;
-                if(item.quantity===0){
-                    removeItem(item.id);
-                }
-            }
-        })
-        setCartItem([...updatedCartArray])
-    }
+    function decrementCartItem(itemId: number) {
+  const updatedCart = cartItem
+    .map((item) => {
+      if (item.id === itemId && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+
+      return item;
+    })
+    .filter((item) => !(item.id === itemId && item.quantity === 1));
+
+  setCartItem(updatedCart);
+}
 
     function totalCartItems(){
         let totalItems : number = 0;
