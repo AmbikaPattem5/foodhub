16aeb323e46ce2b846f28b1794b638ac15fc0a28
AmbikaPattem <ambikapattem5@gmail.com>
2026-09-19 22:10:23 +0530

    done with home styling

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import type { Restaurant } from "../../types/Restaurant";
 import { Restaurants } from "../../services/restaurantData";
-import RestaurantCard from "../../Components/RestaurantDetails";
 import useFavorite from "../../CustomHooks/useFavorite";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
@@ -9,7 +7,7 @@ function Home() {
   const [searchInput, setSearchInput] = useState<string>("");
   const [selectedCuisine, setSelectedCuisine] = useState<string>("");
   // console.log(Restaurants)
-      const {addFavorites,favorites} = useFavorite();
+  const { addFavorites, favorites } = useFavorite();
 
   const navigate = useNavigate();
   function handleSearch(e) {
@@ -52,59 +50,63 @@ function Home() {
   }
   return (
     <div>
-      <div>
+      <div className="flex justify-center gap-8 my-10">
         <section>
           <div>
-            <h4>Delicious food, delivered to your door 🍕</h4>
-            <p>Discover restaurants and delicious meals near you</p>
+            <h6 className="text-red-500">Good Food, Happy People</h6>
+            <h1 className="text-2xl lg:text-6xl font-semibold">Find Your<br/>
+            Next<span className="text-red-500 text-2xl lg:text-6xl font-semibold"> Favorite </span> Meal</h1>
           </div>
           <div>
             <input
               type="text"
               value={searchInput}
               onChange={handleSearch}
+              className="border rounded-md h-10 w-120 mt-6 mb-1"
               placeholder="Search restaurants or cuisines..."
             />
-            {/* {searchInput.trim()!=""&&(
-                    filteredResult.length>0?filteredResult.map((restaurant)=>
-                    <p>{restaurant.restaurantName}</p>
-                    ):<p>"NO Items Found"</p>
-                    )
-            } */}
           </div>
         </section>
       </div>
-      <div>
-        <h4>Popular Cuisines</h4>
+      <div className="flex justify-center gap-3">
+        <h5>Popular : </h5>
         {uniqueList.map((cuisin) => (
-          <button key={cuisin} onClick={() => handleCusine(cuisin)}>{cuisin}</button>
+          <button key={cuisin} className="border rounded h-5 w-20 text-[2px] lg:text-[10px]" onClick={() => handleCusine(cuisin)}>
+            {cuisin}
+          </button>
         ))}
       </div>
+      <div>
       {filteredResult.length > 0 ? (
         <div>
-          <h4>Restaurants near you</h4>
-          <div className="restaurentList">
+          <h1 className="text-lg lg:text-3xl font-semibold px-15">Restaurants near you</h1>
+          <p className="px-3 lg:px-15 text-gray-400 mb-1">Discover best restaurants around you</p>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 px-4 lg:px-16 max-w-75xl mx-auto" >
             {filteredResult.map((restaurant) => (
               <div
                 key={restaurant.id}
                 onClick={() => handleCard(restaurant.id)}
-                className="card"
+                className="relative border border-gray-200 rounded lg:rounded-2xl shadow-lg h-60 w-65"
               >
                 <img
                   src={restaurant.restaurantImage}
-                  alt="image"
-                  height={50}
-                  width={100}
+                  alt="image" className="w-full h-30 object-cover"
+                  
                 />
-                <h6>{restaurant.restaurantName}</h6>
-                <p>
-                  <small>⭐ {restaurant.rating}</small>
-                </p>
-                <p>
-                  <small>{restaurant.deliveryTime} minutes</small>
-                </p>
-                <button onClick={(e)=> {e.stopPropagation(); addFavorites(restaurant.id)}}>{favorites.includes(restaurant.id)? '❤️' :'♡'}</button>
-                
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    addFavorites(restaurant.id);
+                  }}
+                  className="absolute top-1 right-2  hover:bg-red-200 rounded-full text-white"
+                >
+                 <span> {favorites.includes(restaurant.id) ? "❤️" : "♡"}</span>
+                </button>
+                  <span className="rating border border-gray-100 rounded-2xl bg-white text-[12px] text-red-600">⭐ {restaurant.rating}</span>
+                  <span className="time border border-gray-100 rounded-2xl bg-white text-[12px]">{restaurant.deliveryTime} mins</span>
+                <h4 className="font-semibold px-2">{restaurant.restaurantName}</h4>
+                <p className="px-2">{restaurant.cuisine}</p>
+                <h6 className="font-semibold px-2">₹{restaurant.priceForTwo} for two</h6>
               </div>
             ))}
           </div>
@@ -112,6 +114,7 @@ function Home() {
       ) : (
         <p>No Items Found</p>
       )}
+      </div>
     </div>
   );
 }
