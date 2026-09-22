2ff554bd7affcaee83c46f9152821b66f737ad61
mummadi venkatesh prasad <venkateshprasad.mummidi@gmail.com>
2026-09-22 14:04:12 +0530

    deploy configuraions

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/foodhub/',
+  plugins: [react(), tailwindcss()],
+
 })
